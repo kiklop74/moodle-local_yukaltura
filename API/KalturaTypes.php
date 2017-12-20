1901c9be145fcd8a6686897fc94d6f9813636ca6
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2017-12-21 02:47:19 +0900

    Add files via upload

--- API/KalturaTypes.php
+++ API/KalturaTypes.php
@@ -24,12 +24,9 @@
  */
 
 require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
-require_once("KalturaClientBase.php");
+defined('MOODLE_INTERNAL') || die();
 
-if (!defined('MOODLE_INTERNAL')) {
-    // It must be included from a Moodle page.
-    die('Direct access to this script is forbidden.');
-}
+require_once("KalturaClientBase.php");
 
 /**
  * Kaltura Base Restriction class
